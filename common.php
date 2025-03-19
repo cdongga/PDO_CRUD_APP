4a01b67d4652891118ded5ca6c7b6fc86aedf634
cdongga <b00163311@mytudublin.ie>
2025-03-19 18:46:28 +0000

    Add files via upload

---
+++ common.php
@@ -0,0 +1,11 @@
+<?php
+function escape($data) {
+    if ($data === null) {  // Fix for deprecated warning
+        return "";
+    }
+    $data = htmlspecialchars($data, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
+    $data = trim($data);
+    $data = stripslashes($data);
+    return $data;
+}
+?>
